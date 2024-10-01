75b64ebd685c6c6bb23d806db5dfef91109f858c
amary1939@conestogac.on.ca <amary1939@conestogac.on.ca>
2024-09-30 22:18:29 -0400

    changed logo and added hero banner and text in landing page

--- client/src/assets/images.js
+++ client/src/assets/images.js
@@ -1,6 +1,6 @@
-import logoConvo from "./logo.png";
+import logoConvo from "./logoConvo.png";
 import auth from "./auth.png";
 import forgotPass from "./forgotPassword.png"
-import hero from "./hero.png";
+import heroimage from "./heroimage.png";
 
-export { logoConvo, auth, forgotPass, hero };
+export { logoConvo, auth, forgotPass, heroimage };
