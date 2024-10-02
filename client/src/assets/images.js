4988377260ac8c41600f7b084ec4c346182f29a7
amary1939@conestogac.on.ca <amary1939@conestogac.on.ca>
2024-10-02 17:43:09 -0400

    landing page

--- client/src/assets/images.js
+++ client/src/assets/images.js
@@ -7,6 +7,10 @@ import realtime from './realtime.png';
 import videocall from "./videocall.png";
 import fileshare from "./fileshare.png";
 import groupchat from './groupchat.png';
+import sign from './sign.png';
+import startcolab from './startcolab.png';
+import create from './create.png';
+import uparrow from './uparrow.png';
+import downarrow from './down arrow.png';
 
-
-export { logoConvo, auth, forgotPass, heroimage, ai, realtime, videocall, fileshare, groupchat};
+export { logoConvo, auth, forgotPass, heroimage, ai, realtime, videocall, fileshare, groupchat,sign,startcolab,create,uparrow,downarrow };
