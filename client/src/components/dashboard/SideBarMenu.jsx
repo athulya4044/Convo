3e12781f25edb635722fedc93504ae89b977c24b
Joemol Joy <joemoljoy.a@gmail.com>
2024-12-09 16:20:15 -0500

    side bar chnages

--- client/src/components/dashboard/SideBarMenu.jsx
+++ client/src/components/dashboard/SideBarMenu.jsx
@@ -20,6 +20,7 @@ import {
   Eye,
 } from "lucide-react"; 
 import { logoConvo } from "@/assets/images";
+import { LogOut, User, ChevronsDownUp, MoreVertical } from "lucide-react";
 import SearchBar from "@/components/dashboard/SearchBar";
 import CustomChannelPreview from "./CustomChannelPreview";
 import AccountInfo from "./AccountInfo";
